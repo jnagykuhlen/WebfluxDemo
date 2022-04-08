cf2b2e1c7e4ac6a7009607cdd9ddbf0fc2c138a8
Jonas Nagy-Kuhlen <jonas.nagy-kuhlen@andrena.de>
2022-04-08 18:13:09 +0200

    Make GreetingController reactive

--- src/main/java/com/example/webfluxdemo/GreetingController.java
+++ src/main/java/com/example/webfluxdemo/GreetingController.java
@@ -1,6 +1,5 @@
 package com.example.webfluxdemo;
 
-import com.example.webfluxdemo.model.PersonalizedGreeting;
 import com.example.webfluxdemo.services.HtmlTemplateService;
 import com.example.webfluxdemo.services.PersonalizedGreetingService;
 import lombok.AllArgsConstructor;
@@ -9,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @AllArgsConstructor
@@ -21,18 +21,18 @@ public class GreetingController {
     private final HtmlTemplateService htmlTemplateService;
 
     @GetMapping(value = "/hello/{userId}", produces = MediaType.TEXT_HTML_VALUE)
-    public String getHello(@PathVariable int userId) {
-        try {
-            PersonalizedGreeting personalizedGreeting = personalizedGreetingService.personalizeGreeting(userId).block();
-            return htmlTemplateService.createHtml(
-                    personalizedGreeting.getGreeting(),
-                    personalizedGreeting.getName()
-            );
-        } catch(Exception exception) {
-            System.out.println("EXCEPTION: " + exception.getMessage());
-            String errorMessage = "Cannot personalize greeting for user with ID " + userId + "!";
-            return htmlTemplateService.createErrorHtml(errorMessage);
-        }
+    public Mono<String> getHello(@PathVariable int userId) {
+        return personalizedGreetingService.personalizeGreeting(userId)
+                .map(personalizedGreeting ->
+                        htmlTemplateService.createHtml(
+                                personalizedGreeting.getGreeting(),
+                                personalizedGreeting.getName()
+                        ))
+                .onErrorResume(exception -> {
+                    System.out.println("EXCEPTION: " + exception.getMessage());
+                    String errorMessage = "Cannot personalize greeting for user with ID " + userId + "!";
+                    return Mono.just(htmlTemplateService.createErrorHtml(errorMessage));
+                });
     }
 
 }
